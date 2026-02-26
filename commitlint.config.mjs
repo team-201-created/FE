71aaf3ad82807d64b964c0f8a4c6d9cd2c8be127
hyunsik2000 <ryuhsik2000@naver.com>
2026-02-26 18:11:02 +0900

    feat: husky 훅과 commitlint 설정 추가 (#15)
    커밋 전에는 커밋 메시지를 체크, 푸쉬 전에는 린트 오류를 체크 합니다.
    
    #15

---
+++ commitlint.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'style', 'refactor', 'chore', 'add'],
+    ],
+    'subject-case': [0],
+    'header-max-length': [2, 'always', 72],
+    'type-case': [2, 'always', 'lower-case'],
+  },
+};
