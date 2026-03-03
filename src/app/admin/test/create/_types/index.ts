dec82248d95450f904f936df350847b5a1c35137
hyunsik2000 <ryuhsik2000@naver.com>
2026-03-03 14:07:30 +0900

    feat: 테스트 type 추가 (#40)
    #40

---
+++ src/app/admin/test/create/_types/index.ts
@@ -0,0 +1,22 @@
+export interface Option {
+  answer_option_key: string
+  answer_option_text: string
+  sort_order: number
+}
+
+export interface Question {
+  key: string // 내부 체크용 키
+  question_key: string
+  question_text: string
+  selection_type: 'SINGLE' | 'MULTI' | 'PHOTO'
+  is_required: boolean
+  sort_order: number
+  options: Option[]
+}
+
+export interface TestFormData {
+  name: string
+  description: string
+  profiling_type: string
+  questions: Question[]
+}
