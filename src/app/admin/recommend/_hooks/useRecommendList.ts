72ecbfea3c7a5585c74e0a5201f291254c1f4101
soyeon <gkdl0471@gmail.com>
2026-03-05 16:42:02 +0900

    fix: 병합 충돌 마커 제거 (#61)

--- src/app/admin/recommend/_hooks/useRecommendList.ts
+++ src/app/admin/recommend/_hooks/useRecommendList.ts
@@ -1,11 +1,4 @@
 import { useCallback, useState } from 'react'
-<<<<<<< HEAD
-import { ScentMapItemResponse } from '@/app/admin/recommend/_types'
-import { fetchAdminScentMaps } from '@/app/admin/recommend/_api'
-
-export const useRecommendList = () => {
-  const [data, setData] = useState<ScentMapItemResponse[]>([])
-=======
 import {
   ScentMapItemResponse,
   ProductMapItemResponse,
@@ -19,23 +12,17 @@ export const useRecommendList = () => {
   const [data, setData] = useState<
     (ScentMapItemResponse | ProductMapItemResponse)[]
   >([])
->>>>>>> b20e080 (feat: 추천관리 페이지를 TAB-ID 별로 구조화 (#65))
   const [isLoading, setIsLoading] = useState(false)
 
   const getRecommendList = useCallback(async (tabId: string) => {
     setIsLoading(true)
     if (tabId === 'SCENT_MAP') {
-<<<<<<< HEAD
-      setIsLoading(true)
-      const response = await fetchAdminScentMaps()
-=======
       const response = await fetchAdminScentMaps()
       if (response.success) {
         setData(response.data.content)
       }
     } else if (tabId === 'PRODUCT_MAP') {
       const response = await fetchAdminProductMaps()
->>>>>>> b20e080 (feat: 추천관리 페이지를 TAB-ID 별로 구조화 (#65))
       if (response.success) {
         setData(response.data.content)
       }
