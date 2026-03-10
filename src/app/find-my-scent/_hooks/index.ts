99144949b6bdc211b3409bbcda4f7e19cd8e8cd4
soyeon <gkdl0471@gmail.com>
2026-03-10 13:25:32 +0900

    refactor: 퀴즈 단계·답변 상태 훅 분리 (#58)

---
+++ src/app/find-my-scent/_hooks/index.ts
@@ -0,0 +1,7 @@
+/**
+ * find-my-scent 퀴즈·폼·에러 관련 훅
+ */
+export { useProfilingForm } from './useProfilingForm'
+export { useQuizStep } from './useQuizStep'
+export { useErrorPopup } from './useErrorPopup'
+export type { AnswersState } from './useQuizStep'
