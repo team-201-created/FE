d5cd611ebb6764eda840e2f7b3f4864fb600c59a
soyeon <gkdl0471@gmail.com>
2026-03-12 18:11:19 +0900

    refactor: useProductList 제거 후 타입만 productsClient에서 re-export (#119)

--- src/app/products/_hooks/index.ts
+++ src/app/products/_hooks/index.ts
@@ -1,6 +1,5 @@
 /**
- * products 목록·상세 모달 훅
+ * products 상세 모달 훅 및 목록 아이템 타입 (타입은 _api/productsClient에서 re-export)
  */
-export { useSinglesList, useCombinationsList } from './useProductList'
-export type { SingleItem, CombinationItem } from './useProductList'
+export type { SingleItem, CombinationItem } from '../_api/productsClient'
 export { useProductDetailModal } from './useProductDetailModal'
